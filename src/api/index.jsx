376974a3836505efaca3dbe39c565f3c6b430da1
ismail haddad <haddadismail59@gmail.com>
2025-01-09 11:07:32 -0500

    update api key

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -14,7 +14,7 @@ export async function getWeatherData(endpoint, place_id, measurementSystem) {
             units: measurementSystem // Measurement system (e.g., metric or us)
         },
         headers: {
-            'x-rapidapi-key': '326de24f2cmsh77d9cb9c39f6ef1p13ff6djsn342d797418de',
+            'x-rapidapi-key': '49351c56d7msh5263215604f42f8p1da630jsnda8d5cbf383d',
             'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com'
         }
     };
@@ -43,7 +43,7 @@ export async function searchPlaces(text) {
             language: 'en'
         },
         headers: {
-            'x-rapidapi-key': '326de24f2cmsh77d9cb9c39f6ef1p13ff6djsn342d797418de',
+            'x-rapidapi-key': '49351c56d7msh5263215604f42f8p1da630jsnda8d5cbf383d',
             'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com' 
         }
     };
